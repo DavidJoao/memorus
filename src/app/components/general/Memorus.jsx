7f9541335647719a3bb4510740c29f2c0f0771bc
DavidJoao <davidsandoval596@gmail.com>
2025-03-11 20:30:08 -0700

    CREATE: Folder for general components; Navbar and Title

---
+++ src/app/components/general/Memorus.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Memorus = () => {
+  return (
+    <h1 className="text-[45px] lg:text-[65px] font-bold bg-gradient-to-r from-stone-600 to-stone-400 bg-clip-text text-transparent">MemorUs</h1>
+  )
+}
+
+export default Memorus
---
+++ src/app/components/general/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ToggleTheme from "@/app/components/general/ToggleTheme"
+
+const Navbar = () => {
+  return (
+    <div className='h-[3rem] fixed w-full flex items-center p-2 bg-stone-100 dark:bg-black shadow-xl'>
+      <ToggleTheme/>
+    </div>
+  )
+}
+
+export default Navbar
