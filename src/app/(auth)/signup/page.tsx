3bbbc1eb6566dcedb38c8bda45b4f1306a67f7c6
DavidJoao <davidsandoval596@gmail.com>
2025-03-11 20:30:49 -0700

    CREATE: Pages for Signin and Signup

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,8 +1,13 @@
+import SignupForm from '@/app/components/auth/SignupForm'
+import Memorus from '@/app/components/general/Memorus'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='pt-[5rem] w-full h-screen flex flex-col items-center'>
+      <Memorus />
+      <SignupForm />  
+    </div>
   )
 }
 
